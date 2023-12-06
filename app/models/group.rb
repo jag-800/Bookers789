dcf1c541beaac143c6a8d38ed1370c6dbd8befbe
jo.yunji <ec2-user@ip-172-31-18-249.ap-northeast-1.compute.internal>
2023-12-06 05:28:29 +0000

    finish/7c

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,5 +4,13 @@ class Group < ApplicationRecord
   
   validates :name, presence: true
   validates :introduction, presence: true
-  attachment :image, destroy: false
+  has_one_attached :image
+  
+  def get_group_image(weight, height)
+    unless self.image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    self.image.variant(resize_to_fill: [weight,height]).processed
+  end
 end
